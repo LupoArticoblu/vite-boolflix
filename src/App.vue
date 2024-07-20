c5d0f4c538213eba064f5f1e60b8110595ff76e1
LupoArticoblu <wolf94-@libero.it>
2024-07-20 13:46:40 +0200

    paginazione da fixare[trova il num di pag api]

--- src/App.vue
+++ src/App.vue
@@ -25,9 +25,17 @@ export default {
       let apiUrl = isPopular ? `https://api.themoviedb.org/3/${type}/popular` : `${store.apiUrl}${type}`;
 
       return axios.get(apiUrl, { 
-        params : store.apiParams  
+        params :{ 
+          ...store.apiParams,
+          page: isPopular ? 1 : store.apiParams.page
+        }   
       })
         .then(res => {
+          if (type === 'movie') {
+            store.totPagesMovie = res.data.total_pages;
+          } else if (type === 'tv') {
+            store.totPagesTv = res.data.total_pages;
+          }
           console.log(res.data.results); 
           return res.data.results
         })
@@ -35,6 +43,13 @@ export default {
           console.log('error!', err);
           return []; //array vuoto in cASO di errore
         })
+    },
+    changePage(newPage) {
+      if (newPage < 1 || newPage >  (store.type === 'movie' ? store.totPagesMovie : store.totPagesTv)) {
+        return; // se la pagina è fuori dal range, non cambio pagina
+      }
+      store.apiParams.page = newPage; // se non ho errori, cambio pagina
+      this.startSearch(); // ricarica i dati per la nuova pagina
     },
     startSearch() {
       console.log('API Params:', store.apiParams);
@@ -73,9 +88,24 @@ export default {
 <template>
   <div>
     <Appheader @search="startSearch" />
-    <Appmain v-if="store.type === 'all' || store.type === 'movie'" title="Film" :cards="store.movie" />
-    <Appmain v-if="store.type === 'all' || store.type === 'tv'" title="Serie TV" :cards="store.tv" />
+    <Appmain v-if="store.movie.length > 0 && store.type === 'all'" title="Film e serie tv" :cards="store.movie.concat(store.tv)" />
+    <Appmain v-if="store.type === 'movie'" title="Film" :cards="store.movie" />
+    <Appmain v-if="store.type === 'tv'" title="Serie TV" :cards="store.tv" />
+
+    <!-- Controlli di Paginazione -->
+    <div v-if="store.type === 'movie'" class="pagination-controls">
+      <button @click="changePage(store.apiParams.page - 1)" :disabled="store.apiParams.page <= 1">‹‹</button>
+      <span>Pagina {{ store.apiParams.page }} di {{ store.totalPagesMovie }}</span>
+      <button @click="changePage(store.apiParams.page + 1)" :disabled="store.apiParams.page >= store.totalPagesMovie">››</button>
+    </div>
+
+    <div v-if="store.type === 'tv'" class="pagination-controls">
+      <button @click="changePage(store.apiParams.page - 1)" :disabled="store.apiParams.page <= 1">‹‹</button>
+      <span>Pagina {{ store.apiParams.page }} di {{ store.totalPagesTv }}</span>
+      <button @click="changePage(store.apiParams.page + 1)" :disabled="store.apiParams.page >= store.totalPagesTv">››</button>
+    </div>
   </div>
+  
 </template>
 
 
@@ -84,5 +114,14 @@ export default {
   @use './style/vars.scss' as *;
   *{
     cursor: default;
+  }
+  .pagination-controls {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+    button {
+      margin: 0 1rem;
+    }
   }
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -5,9 +5,12 @@ export const store = reactive({
     api_key: 'f2762044b2f9954e6eb69ef3c1675cc9',
     query: '',
     language: 'it-IT',
+    page: 1, // paginazione, pagina corrente
   },
   apiUrl:'https://api.themoviedb.org/3/search/',
   tv: [],
   movie: [],
   type: 'all',
+  totPagesMovie: 1,
+  totPagesTv: 1,
 });
